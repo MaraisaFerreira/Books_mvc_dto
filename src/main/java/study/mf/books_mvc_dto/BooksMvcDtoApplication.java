d70efac13fe47f5fcaee3e87fea8844734095014
MaraisaFerreira <ferreira.maraisacristina@gmail.com>
2026-04-23 09:37:36 -0300

    init

---
+++ src/main/java/study/mf/books_mvc_dto/BooksMvcDtoApplication.java
@@ -0,0 +1,13 @@
+package study.mf.books_mvc_dto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksMvcDtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksMvcDtoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/mf/books_mvc_dto/BooksMvcDtoApplicationTests.java
@@ -0,0 +1,13 @@
+package study.mf.books_mvc_dto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksMvcDtoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
